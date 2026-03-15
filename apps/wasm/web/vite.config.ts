7d4c22dc77d7de52cd6aa9d02c008552d9ca62ea
Yohan Sandun <yohan.sandunk@gmail.com>
2026-03-15 18:06:39 +0530

    update vite conf

--- apps/wasm/web/vite.config.ts
+++ apps/wasm/web/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  // Treat .wasm as a URL asset so Emscripten JS can locate it at runtime
+  base: '/avrion/',
   assetsInclude: ['**/*.wasm'],
   server: {
     headers: {
